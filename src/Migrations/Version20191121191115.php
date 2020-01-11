3041c456ce8b648bff97d51b16ed8d2e8552cd8a
Alex Rock Ancelet <pierstoval@gmail.com>
2020-01-11 20:28:46 +0100

    Remove useless comment in migrations

--- src/Migrations/Version20191121191115.php
+++ src/Migrations/Version20191121191115.php
@@ -16,9 +16,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20191121191115 extends AbstractMigration
 {
     public function getDescription(): string
--- src/Migrations/Version20191121192127.php
+++ src/Migrations/Version20191121192127.php
@@ -16,9 +16,6 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20191121192127 extends AbstractMigration
 {
     public function getDescription(): string
