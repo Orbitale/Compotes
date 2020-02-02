89343441e9936caa5ebd7dcd1d96222943cc60dc
Alex Rock Ancelet <pierstoval@gmail.com>
2020-02-02 11:34:50 +0100

    CS fix

--- src/DataFixtures/OperationFixtures.php
+++ src/DataFixtures/OperationFixtures.php
@@ -1,10 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the Compotes package.
+ *
+ * (c) Alex "Pierstoval" Rock <pierstoval@gmail.com>.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace App\DataFixtures;
 
-
 use App\Entity\Operation;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\ORMFixtureInterface;
 use Orbitale\Component\DoctrineTools\AbstractFixture;
 
@@ -19,7 +29,7 @@ class OperationFixtures extends AbstractFixture implements ORMFixtureInterface
     {
         return [
             [
-                'operationDate' => new \DateTimeImmutable(),
+                'operationDate' => new DateTimeImmutable(),
                 'type' => 'EXAMPLE TYPE',
                 'typeDisplay' => 'Example display type',
                 'details' => 'THIS IS JUST AN EXAMPLE',
