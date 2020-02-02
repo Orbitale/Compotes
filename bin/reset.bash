95d00e3cc924014b83e8ec5dba82427b1cfd59d0
Alex Rock Ancelet <pierstoval@gmail.com>
2020-02-02 11:40:43 +0100

    Add tags sync to reset script

--- bin/reset.bash
+++ bin/reset.bash
@@ -7,3 +7,4 @@ php "${DIR}"/console --no-interaction doctrine:query:sql "truncate migration_ver
 php "${DIR}"/console --no-interaction doctrine:migrations:migrate
 php "${DIR}"/console --no-interaction doctrine:fixtures:load --append
 php "${DIR}"/console --no-interaction operations:import
+php "${DIR}"/console --no-interaction operations:update-tags
