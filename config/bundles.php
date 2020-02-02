b24a7a9f5ede1f902a098062d4635ead49e66f2a
Alex Rock Ancelet <pierstoval@gmail.com>
2020-02-02 11:33:32 +0100

    Enable FixturesBundle in all envs

--- config/bundles.php
+++ config/bundles.php
@@ -23,6 +23,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
