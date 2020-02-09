cb1112e08a41e40a8251a957ea8ef47bfeb586ac
Alex Rock Ancelet <pierstoval@gmail.com>
2020-02-09 13:31:42 +0100

    Add more basic functional tests for login/admin

--- tests/AdminTest.php
+++ tests/AdminTest.php
@@ -1,16 +1,57 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the Compotes package.
+ *
+ * (c) Alex "Pierstoval" Rock <pierstoval@gmail.com>.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App\Tests;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class AdminTest extends WebTestCase
 {
-    public function test root redirects to admin()
+    use BrowserLoginTrait;
+
+    public function test root redirects to admin(): void
     {
         $client = static::createClient();
         $client->request('GET', '/');
 
         $this->assertResponseRedirects('http://localhost/admin/', 302);
+    }
+
+    public function test root admin page while not logged in redirects to login(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/admin/');
+
+        $this->assertResponseRedirects('http://localhost/login', 302);
+    }
+
+    public function test root admin page while logged as standard user returns 403(): void
+    {
+        $client = static::createClient();
+        $this->login($client, 'test-user', ['ROLE_USER']);
+        $client->request('GET', '/admin/');
+
+        $this->assertResponseStatusCodeSame(403);
+    }
+
+    public function test root admin page while logged as admin user redirects to admin homepage(): void
+    {
+        $client = static::createClient();
+        $this->login($client, 'test-user', ['ROLE_ADMIN']);
+        $client->request('GET', '/admin/');
+
+        $this->assertResponseRedirects('/admin/?action=list&entity=TagRule', 302);
+        $client->followRedirect();
+        $this->assertResponseStatusCodeSame(200);
     }
 }
--- tests/Controller/LoginControllerTest.php
+++ tests/Controller/LoginControllerTest.php
@@ -1,18 +1,50 @@
 <?php
 
+declare(strict_types=1);
+
+/*
+ * This file is part of the Compotes package.
+ *
+ * (c) Alex "Pierstoval" Rock <pierstoval@gmail.com>.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace App\Tests\Controller;
 
+use App\Tests\BrowserLoginTrait;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Security\Core\User\InMemoryUserProvider;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
 
 class LoginControllerTest extends WebTestCase
 {
-    public function test successful login()
+    use BrowserLoginTrait;
+
+    public function test successful login(): void
     {
         $client = static::createClient();
         $client->request('GET', '/login');
 
         $this->assertResponseIsSuccessful();
 
-        static::markTestIncomplete('Must be able to create a test database to finish this, but Symfony CLI is not compatible with that for now...');
+        $client->followRedirects();
+
+        $client->submitForm('Sign in', [
+            '_username' => 'admin',
+            '_password' => 'admin',
+        ]);
+
+        $this->assertSelectorTextSame('.user .user-name', 'admin');
+    }
+
+    public function test login page whiled logged in redirects to admin(): void
+    {
+        $client = static::createClient();
+        $this->login($client, static::$container->get(UserProviderInterface::class)->loadUserByUsername('admin'));
+        $client->request('GET', '/login');
+
+        $this->assertResponseRedirects('/admin/', 302);
     }
 }
