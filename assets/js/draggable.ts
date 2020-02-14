e817162f2932f4d8611293205a449e98cc07f7d1
Alex Rock Ancelet <pierstoval@gmail.com>
2020-02-14 22:03:17 +0100

    Make sure draggable is loaded only if necessary

--- assets/js/draggable.ts
+++ assets/js/draggable.ts
@@ -3,22 +3,27 @@
 import '../css/draggable.css';
 import {Sortable} from '@shopify/draggable';
 
-const sortable = new Sortable(document.querySelectorAll('.draggable_container'), {
-    draggable: '.draggable'
-});
+const draggableContainerClass = 'draggable_container';
+const draggableClass = 'draggable';
 
-sortable.on('sortable:stop', () => {
-    document
-        .querySelectorAll('.draggable_container .draggable input')
-        .forEach((input: Element, index: number) => {
-            if (input.hasAttribute('name')) {
-                let name = input.getAttribute('name') || '';
-                if (!name.match(/\[\d+]/)) {
-                    // No DOM modification if attribute was already removed.
-                    return;
+if (document.getElementsByClassName(draggableContainerClass).length) {
+    const sortable = new Sortable(document.querySelectorAll(`.${draggableContainerClass}`), {
+        draggable: `.${draggableClass}`
+    });
+
+    sortable.on('sortable:stop', () => {
+        document
+            .querySelectorAll(`.${draggableContainerClass} .${draggableClass} input`)
+            .forEach((input: Element, index: number) => {
+                if (input.hasAttribute('name')) {
+                    let name = input.getAttribute('name') || '';
+                    if (!name.match(/\[\d+]/)) {
+                        // No DOM modification if attribute was already removed.
+                        return;
+                    }
+                    input.setAttribute('name', name.replace(/\[\d+]/g, '['+index+']'));
                 }
-                input.setAttribute('name', name.replace(/\[\d+]/g, '['+index+']'));
-            }
-        })
-    ;
-});
+            })
+        ;
+    });
+}
