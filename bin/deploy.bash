dfb3cff1fb743594417c3c9d616b60c19c49aa7e
Alex Rock <pierstoval@gmail.com>
2020-03-30 14:50:12 +0200

    Update deploy.bash

--- bin/deploy.bash
+++ bin/deploy.bash
@@ -4,13 +4,17 @@ DIR=$(dirname "$0")
 
 cd "${DIR}"/..
 
+# Install JS dependencies and build assets with Webpack Encore
+npm install
+npm run build
+
+# Install PHP dependencies and deploy the Symfony app
 composer install --no-interaction --no-progress --classmap-authoritative --no-dev --no-scripts
 php bin/console --no-interaction cache:clear --no-warmup
 sudo service php7.4-fpm restart
 php bin/console --no-interaction cache:warmup
 php bin/console --no-interaction assets:install --symlink --relative
 php bin/console --no-interaction doctrine:migrations:migrate
-php bin/console --no-interaction operations:update-tags
 
-npm install
-npm run build
+# Compotes-specific
+php bin/console --no-interaction operations:update-tags
