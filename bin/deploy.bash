3064a634e4f3ca77ffe5a704be827a0da57edf07
Alex Rock Ancelet <pierstoval@gmail.com>
2020-05-04 20:56:05 +0200

    Change deploy script to use init.d
    The advantage here is that it's possible to run it with sudo with no password.

--- bin/deploy.bash
+++ bin/deploy.bash
@@ -11,7 +11,7 @@ npm run build
 # Install PHP dependencies and deploy the Symfony app
 composer install --no-interaction --no-progress --classmap-authoritative --no-dev --no-scripts
 php bin/console --no-interaction cache:clear --no-warmup
-sudo service php7.4-fpm restart
+sudo /etc/init.d/php7.4-fpm restart
 php bin/console --no-interaction cache:warmup
 php bin/console --no-interaction assets:install --symlink --relative
 php bin/console --no-interaction doctrine:migrations:migrate
