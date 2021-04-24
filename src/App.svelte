92856a64ba1d3a90a72666b33b2ece4feb856e87
Alex Rock Ancelet <pierstoval@gmail.com>
2021-04-24 21:05:31 +0200

    Make splash screen disappear on startup

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,20 @@
 <script lang="ts">
 	export let name: string;
+
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+
+		setTimeout(() => {
+			document.getElementById('splash_screen').classList.add('remove');
+		}, 1000);
+		setTimeout(() => {
+			let splash_screen = document.getElementById('splash_screen');
+			let splash_screen_style = document.getElementById('splash_screen_style');
+			splash_screen.parentElement.removeChild(splash_screen);
+			splash_screen_style.parentElement.removeChild(splash_screen_style);
+		}, 4000);
+	})
 </script>
 
 <main>
