2d4d7941bd14a61f5b9589179b7f0240bdcd855b
Alex Rock Ancelet <pierstoval@gmail.com>
2021-04-27 19:59:13 +0200

    Small html/css reorganization

--- src/App.svelte
+++ src/App.svelte
@@ -19,24 +19,4 @@
 
 <style global lang="scss">
 	@import "./style/_app.scss";
-
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -1,3 +1,12 @@
 <main>
     Compotes app, new version!
 </main>
+
+<style lang="scss">
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
+</style>
