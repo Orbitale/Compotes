9ea62f25c16589682bb38364f8e0624b9a6cf2b7
Alex Rock Ancelet <pierstoval@gmail.com>
2021-05-11 20:49:43 +0200

    Add a "yarn both" for development that runs Svelte AND Tauri

---
+++ dev.js
@@ -0,0 +1,17 @@
+const { spawn } = require('child_process');
+
+const yarn_path = process.env.npm_execpath;
+const node_path = process.env.npm_node_execpath;
+
+const create_process = function(name, args) {
+    let child = spawn(node_path, [yarn_path, ...args]);
+
+    child.stdout.on('data', data => process.stdout.write(`${data}`));
+    child.stderr.on('data', data => process.stderr.write(`${data}`));
+    child.on('close', (code) => console.log(`child process ${name} exited with code ${code}`));
+
+    return child;
+}
+
+const frontend = create_process('FRONTEND', ['run', 'dev']);
+const backend = create_process('BACKEND', ['run', 'tauri', 'dev']);
