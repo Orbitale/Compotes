f926a2a872a978349083a65641a0f2a9b738fe66
Alex Rock Ancelet <pierstoval@gmail.com>
2021-05-13 18:11:36 +0200

    Add needsUser() function to redirect automatically to homepage

--- src/auth/current_user.ts
+++ src/auth/current_user.ts
@@ -1,6 +1,7 @@
 // @ts-ignore
 import User from "../struct/User.ts";
 import {writable, get} from "svelte/store";
+import {replace} from 'svelte-spa-router';
 
 const userStore = writable(null);
 
@@ -8,9 +9,19 @@ const getUser = (): User | null => {
     // TODO: Use this later: get(userStore);
     return new User("1", "admin", "admin");
 };
+
+const needsUser = async (): Promise<void> => {
+    const user = getUser();
+
+    if (!user || !user.id) {
+        await replace('#/');
+    }
+};
+
 const setUser = (user: User | null): void => userStore.set(user);
 
 export {
     getUser,
     setUser,
+    needsUser,
 };
