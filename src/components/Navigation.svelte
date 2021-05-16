58852417885dfd256e11fe0a5d3a31c6a33b9901
Alex Rock Ancelet <pierstoval@gmail.com>
2021-05-16 15:44:21 +0200

    Update tags display

--- src/components/Navigation.svelte
+++ src/components/Navigation.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import {getUser} from '../auth/current_user.ts';
+    import active from 'svelte-spa-router/active';
 
     let user = getUser();
 </script>
@@ -15,27 +16,27 @@
         <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav">
                 {#if user}
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#/analytics">Analytics</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#/operations">Operations</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#/bank-accounts">Bank accounts</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#/triage">Triage</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#/tag-rules">Tag rules</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#/tags">Tags</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#/import">Import</a>
-                    </li>
+                    <li class="nav-item"><a class="nav-link" use:active={"/analytics"} href="#/analytics">
+                        Analytics
+                    </a></li>
+                    <li class="nav-item"><a class="nav-link" use:active={"/operations"} href="#/operations">
+                        Operations
+                    </a></li>
+                    <li class="nav-item"><a class="nav-link" use:active={"/bank-accounts"} href="#/bank-accounts">
+                        Bank accounts
+                    </a></li>
+                    <li class="nav-item"><a class="nav-link" use:active={"/triage"} href="#/triage">
+                        Triage
+                    </a></li>
+                    <li class="nav-item"><a class="nav-link" use:active={"/tag-rules"} href="#/tag-rules">
+                        Tag rules
+                    </a></li>
+                    <li class="nav-item"><a class="nav-link" use:active={"/tags"} href="#/tags">
+                        Tags
+                    </a></li>
+                    <li class="nav-item"><a class="nav-link" use:active={"/import"} href="#/import">
+                        Import
+                    </a></li>
                 {/if}
             </ul>
         </div>
--- src/routes/Tags.svelte
+++ src/routes/Tags.svelte
@@ -1,8 +1,27 @@
 <script lang="ts">
     import {needsUser} from '../auth/current_user.ts';
-    import TagsTable from "../components/Tags/TagsTable.svelte";
+    import {getTags} from "../db/tags.ts";
+    import PaginatedTable from "../components/PaginatedTable/PaginatedTable.svelte";
+    import Tag from "../entities/Tag.ts";
+    import FieldToDisplay from "../struct/FieldToDisplay.ts";
+    import {onMount} from "svelte";
 
     needsUser();
+
+    let tags: Tag[] = [];
+
+    let fields = [
+        new FieldToDisplay('id', 'ID'),
+        new FieldToDisplay('name', 'Date'),
+    ];
+
+    onMount(async () => tags = await getTags())
 </script>
 
-<TagsTable />
+{#if tags.length}
+    <PaginatedTable bind:items={tags} fields={fields} />
+{:else}
+    <div class="alert alert-warning">
+        No tags yet.
+    </div>
+{/if}
