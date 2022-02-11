9cbaffad0ca66d7707f9a961edc4a2f0feb56cf1
Alex Rock Ancelet <pierstoval@gmail.com>
2022-02-11 22:54:52 +0100

    Update dev script

--- dev.mjs
+++ dev.mjs
@@ -4,10 +4,8 @@ const yarn_path = process.env.npm_execpath;
 const node_path = process.env.npm_node_execpath;
 
 const create_process = function(name, args) {
-    let child = spawn(node_path, [yarn_path, ...args]);
+    let child = spawn(node_path, [yarn_path, ...args], {stdio: 'inherit'});
 
-    child.stdout.on('data', data => process.stdout.write(data));
-    child.stderr.on('data', data => process.stderr.write(data));
     child.on('close', (code) => console.log(`child process ${name} exited with code ${code}`));
 
     return child;
