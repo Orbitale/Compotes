b278f4af9e03a48190ff41ea18142c0bfec34195
Alex Rock Ancelet <pierstoval@gmail.com>
2022-04-06 15:52:13 +0200

    Use a plugin to copy/paste bootstrap's JS file instead of versioning it

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,17 @@
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
+import path from 'path';
+import fs from 'fs';
+
+const copyPlugin = function (options) {
+	return function () {
+		const targetDir = path.dirname(options.target);
+		if (!fs.existsSync(targetDir)){
+			fs.mkdirSync(targetDir);
+		}
+		fs.writeFileSync(options.target, fs.readFileSync(options.source));
+	};
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,10 +20,28 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+
+		vite: {
+			plugins: [
+				copyPlugin({
+					source:  './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
+					target: './static/bootstrap.min.js',
+				}),
+				copyPlugin({
+					source:  './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map',
+					target: './static/bootstrap.min.js.map',
+				}),
+			],
+		}
 	},
 
-	vite: {}
+	vite: {
+		prebundleSvelteLibraries: true,
+		optimizeDeps: {
+			entries: []
+		},
+	}
 };
 
 export default config;
