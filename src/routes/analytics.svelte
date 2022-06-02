4e2029ea213fbb3fe3612e778adcba1062ebf248
Alex Rock Ancelet <pierstoval@gmail.com>
2022-06-02 18:07:05 +0200

    Continue preparing the work for analytics graphs

--- src/routes/analytics.svelte
+++ src/routes/analytics.svelte
@@ -10,20 +10,9 @@
     let selected_filter: SavedFilter|null = null;
     let operations: Array<Operation> = [];
 
-    const chart_data = {
-        labels: ['label 1', 'label 2', 'label 3', '', ''],
-        datasets: [
-            {
-                label: "Operations",
-                data: [1, 2, 3, 2, 1],
-                backgroundColor: "rgba(255, 99, 132, 0.2)",
-                borderColor: "rgba(255, 99, 132, 1)",
-                borderWidth: 1
-            }
-        ]
-    };
-
-    const chart_options = {};
+    let chart_component = false;
+    let chart_data = {};
+    let chart_options = {};
 
     onMount(() => {
         filters = getSavedFilters('operations');
@@ -36,6 +25,26 @@
         }
 
         operations = await getOperationsForAnalytics(selected_filter);
+
+        let series = [];
+
+        for (const operation of operations) {
+            // todo: add data to series
+        }
+
+        chart_component = Line;
+        chart_data = {
+            labels: ['label 1', 'label 2', 'label 3', 'lab 4', 'lab 5'],
+            datasets: [
+                {
+                    label: "Operations",
+                    data: [1, 2, 3, 2, 1],
+                    backgroundColor: "rgba(255, 99, 132, 0.2)",
+                    borderColor: "rgba(255, 99, 132, 1)",
+                    borderWidth: 1
+                }
+            ]
+        };
     }
 </script>
 
@@ -43,18 +52,24 @@
 
 <hr>
 
-<h2>Available filters:</h2>
-<select id="available_filters" bind:value={selected_filter} on:change={changeFilter}>
-    <option value={null} selected={selected_filter === null}>- Choose a filter -</option>
-    {#each filters as filter}
-        <option value={filter}>
-            {filter.name}
-        </option>
-    {/each}
-</select>
+<div class="row">
+    <label for="available_filters" class="col-form-label col-sm-2">
+        Available filters:
+    </label>
+    <div class="col-sm-10">
+        <select id="available_filters" class="form-control" bind:value={selected_filter} on:change={changeFilter}>
+            <option value={null} selected={selected_filter === null}>- Choose a filter -</option>
+            {#each filters as filter}
+                <option value={filter}>
+                    {filter.name}
+                </option>
+            {/each}
+        </select>
+    </div>
+</div>
 
 {#if selected_filter}
     <h3>Selected: {selected_filter.name}</h3>
 {/if}
 
-<Line data={chart_data} options={chart_options} />
+<svelte:component this={chart_component} data={chart_data} options={chart_options} />
