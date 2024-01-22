a4e2f954727791265678e6fbf38b2930b5fd6ef2
Alex Rock Ancelet <pierstoval@gmail.com>
2024-01-22 17:17:16 +0100

    Add icons to menu links

--- src/lib/crud/Dashboard.ts
+++ src/lib/crud/Dashboard.ts
@@ -1,5 +1,11 @@
 import { DashboardDefinition, UrlAction } from '@orbitale/svelte-admin';
 import Home from 'carbon-icons-svelte/lib/Home.svelte';
+import PiggyBank from "carbon-icons-svelte/lib/PiggyBank.svelte";
+import ChartLogisticRegression from "carbon-icons-svelte/lib/ChartLogisticRegression.svelte";
+import ListBoxes from "carbon-icons-svelte/lib/ListBoxes.svelte";
+import Tag from "carbon-icons-svelte/lib/Tag.svelte";
+import TagGroup from "carbon-icons-svelte/lib/TagGroup.svelte";
+import FetchUpload from "carbon-icons-svelte/lib/FetchUpload.svelte";
 
 import OperationCrud from '$lib/crud/cruds/OperationCrud';
 import BankAccountsCrud from "$lib/crud/cruds/BankAccountsCrud";
@@ -17,13 +23,13 @@ export const dashboard = new DashboardDefinition({
 
     sideMenu: [
         new UrlAction('Homepage', '/', Home),
-        // new UrlAction('Analytics', '/analytics'),
-        new UrlAction('Operations', '/crud/operations/list'),
-        new UrlAction('Tag rules', '/crud/tag-rules/list'),
-        // new UrlAction('Tags', '/crud/tags/list'),
+        // new UrlAction('Analytics', '/analytics', ChartLogisticRegression),
+        new UrlAction('Operations', '/crud/operations/list', ListBoxes),
+        new UrlAction('Tag rules', '/crud/tag-rules/list', TagGroup),
+        // new UrlAction('Tags', '/crud/tags/list', Tag),
         // new UrlAction('Triage', '/crud/triage/list'),
-        new UrlAction('Bank accounts', '/crud/bank-accounts/list'),
-        new UrlAction('Import', '/import'),
+        new UrlAction('Bank accounts', '/crud/bank-accounts/list', PiggyBank),
+        new UrlAction('Import', '/import', FetchUpload),
     ],
 
     cruds: [
